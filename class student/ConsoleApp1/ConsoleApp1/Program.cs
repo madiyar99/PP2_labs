1d8f5a0a18aac8c7cc8177b9c4c6ad536e2f94b8
madiyar99 <35283030+madiyar99@users.noreply.github.com>
2018-01-15 23:13:27 +0600

    class student

---
+++ class student/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example5
+{
+    class Student
+    {
+        public string name;
+        public int age;
+        public float gpa;
+
+        public Student() //Создаем конструктор без значений
+        {
+            name = "Madiyar";
+            age = 18;
+            gpa = 4;
+        }
+
+        public Student(string n, int a, float g) //Создаем конструктор со значениями
+        {
+            name = n;
+            age = a;
+            gpa = g;
+        }
+
+        public override string ToString() //Выводим все данные в строку, при вызове Console.WriteLine(название класса); 
+        {
+            return name + " " + age + " " + gpa;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student(); //Создаем класс без значений
+            s.name = "Dias"; //Даем значение 
+            Console.WriteLine(s); 
+
+
+            Student s2 = new Student("Mamut", 18, 3); //Создаем класс со значениями
+            Console.WriteLine(s2); 
+            Console.ReadKey();
+        }
+    }
+}
