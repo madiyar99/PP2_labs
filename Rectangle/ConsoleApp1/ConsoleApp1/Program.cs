a7c149d5ea27729bcd8fab67041af4cdc74307b8
madiyar99 <35283030+madiyar99@users.noreply.github.com>
2018-01-16 00:52:33 +0600

    Rectangle

---
+++ Rectangle/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            string[] arr = s.Split(' ');
+
+            int length = int.Parse(arr[0]);
+            int width = int.Parse(arr[1]);
+            
+            Rectangle c = new Rectangle(length, width);
+
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rectangle/ConsoleApp1/ConsoleApp1/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Rectangle
+    {
+        public int width;
+        public int length;
+        public int area;
+
+        public Rectangle()
+        {
+            length = 10;
+            width = 5;
+        }
+
+        public Rectangle(int length, int width)
+        {
+            this.width = width;
+            this.length = length;
+            findArea();
+        }
+
+        public void findArea()
+        {
+            area = width * length;
+        }
+
+
+        public override string ToString()
+        {
+            return "width = " + width + "\nlength = " + length + "\narea = " + area;
+        }
+
+
+    }
+}
