ce50579d42a5808fa0904b1b2f51d54ae95b2702
madiyar99 <35283030+madiyar99@users.noreply.github.com>
2018-02-07 00:23:35 +0600

    Far manager

---
+++ лаб 2/работа с файлами/3/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"C:\Users\Asus\Desktop\PP2_labs_local"); //Создаем экземпляр и прописываем путь к папке
+            DirectoryInfo[] ds = directory.GetDirectories(); // Записываем в массив ds все папки из этого пути
+            FileInfo[] fs = directory.GetFiles(); // Записываем в массив fs все файлы из этого пути
+            /*
+            foreach (DirectoryInfo d in ds)
+            {
+                Console.WriteLine(d.Name);
+            }
+            Console.WriteLine();
+            foreach (FileInfo f in fs)
+            {
+                Console.WriteLine(f.Name);
+            }
+            */
+
+            FileSystemInfo[] fss = directory.GetFileSystemInfos(); // Записываем в ммассив fss и папки и файлы 
+            foreach (FileSystemInfo f in fss) // Выводим все, что есть в массиве fss
+            {
+                if (f.GetType() == typeof(DirectoryInfo)) //Если это папка, то выводим в таком виде
+                {
+                    Console.WriteLine("Directory: " + f.Name);
+                }
+                else
+                {
+                    Console.WriteLine("File: " + f.FullName); // Выводим весь путь к файлу
+                }
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ лаб 2/работа с файлами/4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.Clear();
+
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Up");
+                }
+                if (keyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Down");
+                }
+                if (keyInfo.Key == ConsoleKey.LeftArrow)
+                {
+                    Console.BackgroundColor = ConsoleColor.Magenta;
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("Left");
+                }
+                if (keyInfo.Key == ConsoleKey.RightArrow)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("Right");
+                }
+                if (keyInfo.Key == ConsoleKey.Escape)
+                    break;
+                if (keyInfo.Key == ConsoleKey.Enter)
+                    Console.Clear();
+            }
+        }
+    }
+}
